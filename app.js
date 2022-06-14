d8c496dee89f9852e3553e791f40b9919fa506c8
Tova Jertfelt <tovizitan@gmail.com>
2022-06-14 19:48:49 +0200

    lagt till css och mixins

--- app.js
+++ app.js
@@ -2,26 +2,26 @@ const express = require("express");
 const app = express();
 
 
+const mongoose = require ("mongoose");
+const dbURI = "mongodb+srv://test:test@bowling.fjtiu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+mongoose.connect(dbURI, {useNewUrlParser: true})
+.catch(error => console.log(error));
 //*-----------middleware och statiska filer
-//1. access till css & bilder
+// access till css & bilder
 app.use(express.static("public"));
-//2. för form data 
 app.use(express.urlencoded({extended: true}))
-//3. formula för next och response/requests så programmet inte bara fastnar på första funktionen
+//formula för next och response/requests så programmet inte bara fastnar på första funktionen
 app.use((request, response, next) =>{
   response.locals.path = request.path;
   next();
 })
-app.set('views', path.join(__dirname, 'views'));
+
 app.set("view engine", "ejs");
 
 //--------startsida:
 app.get("/", (request, response) => {
   response.render("index", {title: "StudieStunden"});
 })
-
-
-
 
 
 app.listen(3000);
