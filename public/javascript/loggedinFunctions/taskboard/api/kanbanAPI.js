2fcc6948137a1a719bdd6171ed1b7e4263592386
Tova Jertfelt <tovizitan@gmail.com>
2022-06-22 15:08:56 +0200

    börjat med funktioner för taskboard

--- public/javascript/loggedinFunctions/taskboard/api/kanbanAPI.js
+++ public/javascript/loggedinFunctions/taskboard/api/kanbanAPI.js
@@ -1,10 +1,44 @@
-export default class KanbanAPI {
+class KanbanAPI {
+  
+  static insertItem(columnId, content) {
+    const data = read();
+    const kanbanColumn = data.find(column => column.id == columnId);
+    const item = {
+      id: Math.floor(Math.random() * 100000),
+      content
+    };
+    if (!kanbanColumn){
+      throw new Error("Column does not exist");
+    }
+    kanbanColumn.items.push(item);
+    save(data);
+  }
   static getItems(columnId){
     const kanbanColumn = read().find(column => column.id == columnId);
-    if (!column){
+    if (!kanbanColumn){
       return [];
     }
   }
+
+  static updateItem(itemId, newProps){
+    const data = read();
+    const [item, currentColumn] = (() => {
+			for (const column of data) {
+				const item = kanbanColumn.items.find(item => item.id == itemId);
+
+				if (item) {
+					return [item, column];
+				}
+			}
+		})();
+  }
+
+}
+const Kanban = new KanbanAPI();
+
+
+const save = (data) => {
+  localStorage.setItem("kanban-data", JSON.stringify(data));
 }
 
 const read = () => {
@@ -26,6 +60,3 @@ const read = () => {
   return JSON.pase(json);
 }
 
-const save = (data) => {
-  localStorage.setItem("kanban-data", JSON.stringify(data));
-}
