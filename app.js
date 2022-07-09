f580313785bdd8b741f532703fc17dc5573f4938
Tova Jertfelt <tovizitan@gmail.com>
2022-07-09 14:19:52 +0200

    får internal server 500 när jag kör en get-request från mongoDB.

--- app.js
+++ app.js
@@ -1,39 +1,12 @@
+//**Installations */
+
 const express = require('express');
 const dotenv = require("dotenv").config();
 const app = express();
-const path = require("path");
-const bodyParser = require("body-parser");
-const fs = require("fs");
-const multer = require("multer");
 const _ = require("lodash");
 const mongoose = require("mongoose");
-const User = require("./models/users.js");
-
-app.use(bodyParser.urlencoded({extended:true}));
-
-
-
-const storage = multer.diskStorage({destination: function(req, fule,cb){ cb(null, "uploads")},
-filename: function(req, file,cb){cb(null, file.fieldname + "-" + Date.now())}
-})
-
-const uploadimg = multer({storage: storage});
-
-
-//mongoose connection:
-const USER = process.env.DB_USER;
-const PASS = process.env.DB_PASSWORD;
-const port = process.env.PORT;
-
-const dbURI = "mongodb+srv://"+USER+":"+PASS+"@studiestunden.v4y2zyx.mongodb.net/?retryWrites=true&w=majority";
-
-
-
-mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedToPology: true})
-.then((result) => {app.listen(port)
-  console.log("connected")}
-)
-.catch((error) => console.log("-------ERROR CONNECTING " + error))
+const User = require("./models/users");
+const morgan = require("morgan");
 
 
 // register view engine
@@ -41,90 +14,151 @@ app.set('view engine', 'ejs');
 
 // middleware & static files
 app.use(express.static('public'));
-
-
-app.use((req, res, next) => {
-  console.log('new request made:');
-  console.log('host: ', req.hostname);
-  console.log('path: ', req.path);
-  console.log('method: ', req.method);
-  next();
-});
-
-app.get("/registrera", (req, res) =>{
-
-})
-
+app.use(express.urlencoded({extended:true}));
+app.use(morgan('dev'));
 app.use((req, res, next) => {
   res.locals.path = req.path;
   next();
 });
 
+//**-------mongoose connection:--------*/
+const USER = process.env.DB_USER;
+const PASS = process.env.DB_PASSWORD;
+const port = process.env.PORT;
+
+const dbURI = "mongodb+srv://"+USER+":"+PASS+"@studiestunden.v4y2zyx.mongodb.net/?retryWrites=true&w=majority";
+
+mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedToPology: true})
+.then((result) => {app.listen(port)
+  console.log("connected to database")}
+)
+.catch((error) => console.log("-------ERROR CONNECTING " + error))
+
+
+//**-----------------ROUTES----------  */
+
+//?-------HOMEPAGE
 app.get('/', (req, res) => {
-  res.render('index', { title: 'Start'});
+  res.render('index', { title: 'Studiestunden'});
 });
 
-app.get("/skapa", (req, res) => {
-  console.log("connected")
-  res.render("../views/userInterface/createUser.ejs",{ title: "Registrera dig"})
+
+app.post("/users", (req, res) => {  
+  // console.log(req.body)
+  const user = new User(req.body);
+  user.save()
+  .then((result) => {
+    console.log("resultatet är här: " + result)
+    res.redirect("/medlemmar");
+  })
+  .catch((err) => {
+    console.log(err)
+  })
 })
 
+//!testing
+app.get("/medlemmar", (req, res) => {
+  User.find().then(result =>{
+  // console.log("THIS IS ALL THE RESULTS:" + result)
+  res.render("../views/partials/test"), {user: result, title: "Alla medlemmar"}
+   })
+   .catch(err => {
+    console.log(err)
+   })
+})
+
+//?---------REGISTER
+app.get("/skapa", (req, res) => {
+  res.render("../views/userInterface/createUser",{ title: "Registrera dig!"})
+})
+
+
+
+//?---------LOGGED IN DETAILS
+app.get("/detaljer", (req, res) => {
+  const id = req.params.id;
+ 
+  // User.findById(id).then(result => {
+  //   console.log(result);
+  // //   res.render("../views/userInterface/details.ejs", 
+  // //   {medlem: result, title: "Dina detaljer"})
+  // // }).catch(err => {
+  // //   console.log(err)
+  // // })
+  // })
+})
+
+//?---------LOGIN
 app.get("/login", (req, res) => {
   res.render("../views/userInterface/login.ejs",{ title: "Logga in"})
 })
 
-app.get("/detaljer", (req, res) => {
-  res.render("../views/userInterface/details.ejs",{ title: "Dina detaljer"})
-})
-
+//?---------LOGGED IN TASKBOARD
 app.get("/taskboard", (req, res) => {
   res.render("../views/taskboard/taskboard.ejs",{ title: "Din anslagstavla"})
 })
 
 
-app.post("/users-register", uploadimg.single("Profile"),(req, res) => {
-
-  //profilepic
-  let profileimg = fs.readFileSync(req.file.path);
-  let encodedImg = profileimg.toString("base64");
-  let finalImg = {
-      contentType: req.file.mimetype,
-      image: new Buffer(encodedImg, "base64")
-  }
-
-  const newUser = new User(request.body, finalImg, function (err,result){
-    if(err){
-      console.log(err);
-    }else{
-      console.log(result.profileimg.Buffer);
-      console.log("Saved to database");
-      res.contentType(finalImg.contentType);
-      res.send(finalImg.image);
-    } 
-  })
-  newUser.save().then(() => {
-    res.redirect("/");
-  })
-  .catch((error) => {
-    console.log(error);
-  })
-  //   //usual suspects:
-  // User.create(finalImg, function (err,result){
-  //   if(err){
-  //     console.log(err);
-  //   }else{
-  //     console.log(result.profileimg.Buffer);
-  //     console.log("Saved to database");
-  //     res.contentType(finalImg.contentType);
-  //     res.send(finalImg.image);
-  //   }
-  // })
-})
-
-
-
-
-// 404 page
+//?---------- 404 page
 app.use((req, res) => {
   res.status(404).render('404', { title: '404' });
 });
+
+
+
+
+
+
+//***upload profile pic (in development)
+// const bodyParser = require("body-parser");
+// app.use(bodyParser.urlencoded({extended:true}));
+
+
+// const storage = multer.diskStorage({destination: function(req, fule,cb){ cb(null, "uploads")},
+// filename: function(req, file,cb){cb(null, file.fieldname + "-" + Date.now())}
+// })
+
+// const uploadimg = multer({storage: storage});
+
+
+// app.post("/users-register", uploadimg.single("Profile"),(req, res) => {
+
+//   //profilepic
+//   let profileimg = fs.readFileSync(req.file.path);
+//   let encodedImg = profileimg.toString("base64");
+//   let finalImg = {
+//       contentType: req.file.mimetype,
+//       image: new Buffer(encodedImg, "base64")
+//   }
+
+//   const newUser = new User(request.body, finalImg, function (err,result){
+//     if(err){
+//       console.log(err);
+//     }else{
+//       console.log(result.profileimg.Buffer);
+//       console.log("Saved to database");
+//       res.contentType(finalImg.contentType);
+//       res.send(finalImg.image);
+//     } 
+//   })
+//   newUser.save().then(() => {
+//     res.redirect("/");
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   })
+//   //   //usual suspects:
+//   // User.create(finalImg, function (err,result){
+//   //   if(err){
+//   //     console.log(err);
+//   //   }else{
+//   //     console.log(result.profileimg.Buffer);
+//   //     console.log("Saved to database");
+//   //     res.contentType(finalImg.contentType);
+//   //     res.send(finalImg.image);
+//   //   }
+//   // })
+// })
+
+
+
--- models/users.js
+++ models/users.js
@@ -22,5 +22,5 @@ const userSchema = new Schema({
 {timestamps: true}
 );
 
-const User = mongoose.model("User", userSchema);
+const User = mongoose.model("Users", userSchema);
 module.exports = User;
