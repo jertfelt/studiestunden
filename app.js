98784290ef06c76f5ad1287064d5c5044b9dd9e9
Tova Jertfelt <tovizitan@gmail.com>
2022-07-09 12:29:34 +0200

    bugg med 'registrera' ordnat, btte till /skapa, vet ej varför det inte fungerade förut

--- app.js
+++ app.js
@@ -11,12 +11,23 @@ const User = require("./models/users.js");
 
 app.use(bodyParser.urlencoded({extended:true}));
 
+
+
+const storage = multer.diskStorage({destination: function(req, fule,cb){ cb(null, "uploads")},
+filename: function(req, file,cb){cb(null, file.fieldname + "-" + Date.now())}
+})
+
+const uploadimg = multer({storage: storage});
+
+
 //mongoose connection:
 const USER = process.env.DB_USER;
 const PASS = process.env.DB_PASSWORD;
 const port = process.env.PORT;
 
 const dbURI = "mongodb+srv://"+USER+":"+PASS+"@studiestunden.v4y2zyx.mongodb.net/?retryWrites=true&w=majority";
+
+
 
 mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedToPology: true})
 .then((result) => {app.listen(port)
@@ -31,15 +42,47 @@ app.set('view engine', 'ejs');
 // middleware & static files
 app.use(express.static('public'));
 
-const storage = multer.diskStorage({destination: function(req, fule,cb){ cb(null, "uploads")},
-filename: function(req, file,cb){cb(null, file.fieldname + "-" + Date.now())}
+
+app.use((req, res, next) => {
+  console.log('new request made:');
+  console.log('host: ', req.hostname);
+  console.log('path: ', req.path);
+  console.log('method: ', req.method);
+  next();
+});
+
+app.get("/registrera", (req, res) =>{
+
 })
 
-const uploadimg = multer({storage: storage});
+app.use((req, res, next) => {
+  res.locals.path = req.path;
+  next();
+});
+
+app.get('/', (req, res) => {
+  res.render('index', { title: 'Start'});
+});
+
+app.get("/skapa", (req, res) => {
+  console.log("connected")
+  res.render("../views/userInterface/createUser.ejs",{ title: "Registrera dig"})
+})
+
+app.get("/login", (req, res) => {
+  res.render("../views/userInterface/login.ejs",{ title: "Logga in"})
+})
+
+app.get("/detaljer", (req, res) => {
+  res.render("../views/userInterface/details.ejs",{ title: "Dina detaljer"})
+})
+
+app.get("/taskboard", (req, res) => {
+  res.render("../views/taskboard/taskboard.ejs",{ title: "Din anslagstavla"})
+})
+
 
 app.post("/users-register", uploadimg.single("Profile"),(req, res) => {
-
-
 
   //profilepic
   let profileimg = fs.readFileSync(req.file.path);
@@ -79,42 +122,7 @@ app.post("/users-register", uploadimg.single("Profile"),(req, res) => {
 })
 
 
-app.use((req, res, next) => {
-  console.log('new request made:');
-  console.log('host: ', req.hostname);
-  console.log('path: ', req.path);
-  console.log('method: ', req.method);
-  next();
-});
 
-app.get("/registrera", (req, res) =>{
-
-})
-
-app.use((req, res, next) => {
-  res.locals.path = req.path;
-  next();
-});
-
-app.get('/', (req, res) => {
-  res.render('index', { title: 'Start'});
-});
-
-app.get("/registrera", (req, res) => {
-  res.render("../views/userInterface/createUser.ejs",{ title: "Registrera dig"})
-})
-
-app.get("/login", (req, res) => {
-  res.render("../views/userInterface/login.ejs",{ title: "Logga in"})
-})
-
-app.get("/detaljer", (req, res) => {
-  res.render("../views/userInterface/details.ejs",{ title: "Dina detaljer"})
-})
-
-app.get("/taskboard", (req, res) => {
-  res.render("../views/taskboard/taskboard.ejs",{ title: "Din anslagstavla"})
-})
 
 // 404 page
 app.use((req, res) => {
